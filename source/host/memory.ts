4b6a6f2511f3a2191110bd049a74c2951c01eebc
vissicchioj <60723403+vissicchioj@users.noreply.github.com>
2022-10-07 13:01:34 -0400

    small fix in memory and PCB file addition

--- source/host/memory.ts
+++ source/host/memory.ts
@@ -3,9 +3,7 @@ module TSOS
 {
     //variables
     var i: number = 0x00;
-    var length: number = 0x100;
-    var address: string;
-    var contains: string;
+
 
     export class Memory
     {
@@ -13,10 +11,9 @@ module TSOS
         // create the private array of memory
         private memArray = [];
 
-        constructor()
+        constructor(size: number)
         {
-            //fills up the private array with 0x00
-            for(i = 0x00; i < length; i++)
+            for(i = 0x00; i < size; i++)
             {
                 this.memArray[i] = 0x00;
             }
@@ -26,11 +23,10 @@ module TSOS
         //reset all members in the array to be 0x00
         reset()
         {
-            for(i = 0x00; i < length; i++) 
+            for(i = 0x00; i < this.memArray.length; i++) 
             {
                 this.memArray[i] = 0x00;
             }
         }
-
     }
 }
