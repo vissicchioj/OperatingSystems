5e8039b39cc8089e6406e268ea1c6135664bf6e4
vissicchioj <60723403+vissicchioj@users.noreply.github.com>
2022-10-07 13:37:02 -0400

    Modelled the PCB initialization after the CPU implementation

--- distrib/os/pcb.js
+++ distrib/os/pcb.js
@@ -1,1 +1,29 @@
+var TSOS;
+(function (TSOS) {
+    class ProcessControlBlock {
+        constructor(pid = 0, state = '', location = '', priority = 0, pc = 0, acc = 0, xreg = 0, yreg = 0, zflag = 0) {
+            this.pid = pid;
+            this.state = state;
+            this.location = location;
+            this.priority = priority;
+            this.pc = pc;
+            this.acc = acc;
+            this.xreg = xreg;
+            this.yreg = yreg;
+            this.zflag = zflag;
+        }
+        init() {
+            this.pid = 0;
+            this.state = '';
+            this.location = '';
+            this.priority = 0;
+            this.pc = 0;
+            this.acc = 0;
+            this.xreg = 0;
+            this.yreg = 0;
+            this.zflag = 0;
+        }
+    }
+    TSOS.ProcessControlBlock = ProcessControlBlock;
+})(TSOS || (TSOS = {}));
 //# sourceMappingURL=pcb.js.map
--- source/os/pcb.ts
+++ source/os/pcb.ts
@@ -1,4 +1,33 @@
 module TSOS
 {
-    
+    export class ProcessControlBlock
+    {
+        constructor
+            (
+            public pid: number = 0,
+            public state: String = '',
+            public location: String = '',
+            public priority: number = 0,
+            public pc: number = 0,
+            public acc: number = 0,
+            public xreg: number = 0,
+            public yreg: number = 0,
+            public zflag: number = 0,
+            ) 
+        {
+
+        }
+
+        public init(): void {
+            this.pid = 0;
+            this.state = '';
+            this.location = '';
+            this.priority = 0;
+            this.pc = 0;
+            this.acc = 0;
+            this.xreg = 0;
+            this.yreg = 0;
+            this.zflag = 0;
+        }
+    }
 }
